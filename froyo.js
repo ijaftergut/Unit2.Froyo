994764c43757fabea4e9ea366a4edf13f6aa81be
Isaac Aftergut <ijagut@vt.edu>
2023-08-18 14:01:05 -0400

    first commit

---
+++ froyo.js
@@ -0,0 +1,22 @@
+const list = window.prompt("What flavors would you like? (ex. coffee, strawberry, vanilla)");
+let array = list.split(',')
+
+function getFlavor(str){
+    const flavors = {};
+    for(let i = 0; i < str.length; i++){
+        const flavor = str[i]
+        if(flavors[flavor]=== undefined){
+            flavors[flavor] = 0
+        }
+        flavors[flavor]++
+    }
+    return flavors
+}
+flavors = getFlavor(array)
+console.log(flavors)
+
+let sum = 0
+for(let key in flavors){
+    sum = sum + (flavors[key])
+}
+console.log(sum)
